9b5d06ddded49881e9fe5c88cef67a2f306b2e03
ERKDRN59 <erichka2005@gmail.com>
2022-03-18 10:53:16 +0200

    New maps and basics!
    Added: 2 maps!
    Compacted the code
    Made the basic orientation

--- lesson2-game.py
+++ lesson2-game.py
@@ -1,1 +1,35 @@
-print("Hello!")
+
+lmap1 = [
+    [12,0,1,0],
+    [1,0,1,0],
+    [1,0,1,0],
+    [1,0,0,24]
+        ]
+lmap2 = [
+    [12,1,0,0],
+    [0,0,0,0],
+    [1,1,0,0],
+    [0,0,0,24]
+        ]
+map = lmap1
+startposX = 0
+startposY = 0
+def move(step, playerposX, playerposY):
+    print("current step: ", step)
+    print("Current position X:", playerposX)
+    print("Current position Y:", playerposY)
+    playerposX = startposX
+    playerposY = startposY
+    can_move_right = playerposX <= 2 and map[playerposY][playerposX+1] == 0
+    can_move_down = playerposY <= 2 and map[playerposY+1][playerposX] == 0
+    
+    
+    if can_move_right:
+        print("Going right")
+        playerposX += 1
+    if can_move_down:
+        print("Going down")
+        playerposY += 1
+    return [playerposX, playerposY]
+newpos = move(1,startposX, startposY)
+newpos = move(2, newpos[0], newpos[1])
